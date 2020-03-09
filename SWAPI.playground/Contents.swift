cc149c04b4fd33edba5be1106f61a2ddba929096
DanielMR <jadaniel54@gmal.com>
2020-03-09 16:00:50 -0600

    final

--- SWAPI.playground/Contents.swift
+++ SWAPI.playground/Contents.swift
@@ -1,25 +1,25 @@
 import Foundation
-/*
- Remember that the "Codable" types are String, Int, Double, Date, Data and URL.
- Because of this, we can make `films` an array of URL instead of String.
- */
+
+
 struct Person: Decodable {
-    let name: String
+  let  name: String
     let films: [URL]
 }
-
-struct Film: Decodable {
+struct Film: Codable {
     let title: String
+    let opening_crawl: String
+    let release_date: String
 }
-
 class SwapiService {
     
-    static private let baseURL = URL(string: "https://swapi.co/api")
-    static let peopleEndpoint = "people"
+    static private let baseURL = URL(string: "https://swapi.co/api/")
+    static private let peopleEndPoint = "people/"
+    
+    
     
     static func fetchPerson(id: Int, completion: @escaping (Person?) -> Void) {
         guard let baseURL = baseURL else { return completion(nil) }
-        let peopleURL = baseURL.appendingPathComponent(peopleEndpoint)
+        let peopleURL = baseURL.appendingPathComponent(peopleEndPoint)
         let finalURL = peopleURL.appendingPathComponent("\(id)")
         print(finalURL)
         URLSession.shared.dataTask(with: finalURL) { data, _, _ in
@@ -36,30 +36,60 @@ class SwapiService {
         }.resume()
     }
     
-    static func fetchFilm(url: URL, completion: @escaping (Film?) -> Void) {
-        
-        URLSession.shared.dataTask(with: url) { data, _, _ in
-            guard let data = data else { return completion(nil) }
-            do {
-                let film = try JSONDecoder().decode(Film.self, from: data)
-                completion(film)
-            } catch {
-                return completion(nil)
-            }
-            
-        }.resume()
-    }
-}
+
+//    static func fetchPerson(id: Int, completion: @escaping (Person?) -> Void) {
+//        //1) Prepare URL
+//        guard let baseURL = baseURL else {return completion(nil)}
+//        let peopleURL = baseURL.appendingPathComponent(peopleEndPoint)
+//        let finalURL = peopleURL.appendingPathComponent("\(id)")
+//        print(finalURL)
+//
+//        //2) Contact Server
+//        URLSession.shared.dataTask(with: peopleURL) { (data, _, error) in
+//
+//        //3) Hamdle Errors
+//            if let error = error {
+//                print(error, error.localizedDescription)
+//                return completion(nil)
+//            }
+//
+//        //4) Check for data
+//            guard let data = data else {return completion(nil)}
+//
+//        //5) Decode Person from JSON
+//            do{
+//                let decodeData = try JSONDecoder().decode(Person.self, from: data)
+//                return completion(decodeData)
+//            }catch {
+//                print(error, error.localizedDescription)
+//                return completion(nil)
+//            }
+//        }.resume()
+//    }
+    
+    static func fetchFilm(url: URL, completion: @escaping (Film?) -> Void ) {
+        URLSession.shared.dataTask(with: url) { data, _, error in
+                guard let data = data else { return completion(nil) }
+                do {
+                    let film = try JSONDecoder().decode(Film.self, from: data)
+                    completion(film)
+                } catch {
+                    return completion(nil)
+                }
+                
+            }.resume()
+        }
+}// End of Class
 
 func fetchFilm(url: URL) {
     SwapiService.fetchFilm(url: url) { (film) in
         if let film = film {
-            print(film)
+            print(film.title)
         }
     }
 }
 
-SwapiService.fetchPerson(id: 10) { (person) in
+SwapiService.fetchPerson(id: 11) { (person) in
     if let person = person {
         print(person.name)
         for film in person.films {
